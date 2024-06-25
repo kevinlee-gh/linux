084848c1513f4b3c033a375baac9d058d13e4463
kienlt <kienlt@topcv.vn>
2024-06-25 17:28:10 +0700

    refactor

--- init.bash
+++ init.bash
@@ -1,3 +1,5 @@
+FILE_DIR=$(dirname $0)
+
 if [ ! -d ~/.local ]; then
     mkdir -p ~/.local
     cd ~/.local
@@ -6,7 +8,11 @@ if [ ! -d ~/.local ]; then
 fi
 
 # Get personal bins
-./terminal/link_bin.sh
+for BIN_NAME in $(ls ${FILE_DIR}/terminal/bin); do
+    BIN_FILE_DIR=$(realpath "${FILE_DIR}/terminal/bin/${BIN_NAME}" | xargs dirname)
+    BIN_FILE="${BIN_FILE_DIR}/${BIN_NAME}"
+    ln -sf "${BIN_FILE}" ~/.local/bin
+done
 
 # Get personal bashrc
 sed -i '/^# >>> PERSONAL PATH$/,/^# <<< PERSONAL PATH$/d' ~/.bashrc
