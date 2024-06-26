d571a4c4a13a99f7e85b0aaa0ecfd84c34dd9a0e
kienlt <kienlt@topcv.vn>
2024-06-26 15:59:16 +0700

    add zellij theme + use tilda as terminal emulator

--- terminal/setup.sh
+++ terminal/setup.sh
@@ -25,5 +25,16 @@ echo "  + Add '~/.config':"
 
 for CONFIG_DIRNAME in $(ls ${FILE_DIR}/config); do
     echo "    * ${CONFIG_DIRNAME}"
-    ln -sf "${FILE_DIR}/config/${CONFIG_DIRNAME}" ~/.config/${CONFIG_DIRNAME}
+
+    SOURCE_DIR=${FILE_DIR}/config/${CONFIG_DIRNAME}
+    DESTINATION_DIR=~/.config/${CONFIG_DIRNAME}
+    if [ -L ${DESTINATION_DIR} ]; then 
+        rm ${DESTINATION_DIR}
+    elif [ -d ${DESTINATION_DIR} ]; then
+        echo "WARN: '${DESTINATION_DIR}' is existing as a dir. Save a backup at ${DESTINATION_DIR}.bak"
+        mv ${DESTINATION_DIR} ${DESTINATION_DIR}.bak
+    fi
+
+    ln -s ${SOURCE_DIR} ${DESTINATION_DIR}
+
 done
