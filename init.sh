91d3a8c4a1ae09715809131142363fb307d0c267
kienlt <kien.letrung610@gmail.com>
2024-08-22 11:20:22 +0700

    add first argument to init.sh

--- init.sh
+++ init.sh
@@ -1,4 +1,14 @@
-ROOT_DIR=$(dirname `realpath $0`)
+ROOT_DIR=$(dirname $(realpath $0))
+
+if [[ -z "$1" ]]; then
+  echo 'Missing first arg for `DEVICE_TYPE`'
+  exit 1
+elif [[ "$1" != "PC" ]] && [[ "$1" != "SERVER" ]]; then
+  echo "First arg - \`DEVICE_TYPE\` must be \`PC\` or \`SERVER\`, not \`${1}\`"
+  exit 1
+else 
+  DEVICE_TYPE=${1}
+fi
 
 # Create personal dirs
 ${ROOT_DIR}/home/init.sh
