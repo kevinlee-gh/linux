9bafbbe247d5bbddc9adc5c737785cf9f9f0493e
kienlt <kien.letrung610@gmail.com>
2024-08-22 11:50:31 +0700

    add check distro_id

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
 export ROOT_DIR=$(dirname $(realpath $0))
 
+DISTRO_ID=$(lsb_release --id | sed 's/Distributor ID:\s//')
+if [[ ${DISTRO_ID} != "Ubuntu" ]]; then
+  echo "Not supported for \`${DISTRO_ID}\`"
+  exit 1
+fi
+
 if [[ -z "$1" ]]; then
   echo 'Missing first arg for `DEVICE_TYPE`'
   exit 1
@@ -10,6 +16,8 @@ else
   export DEVICE_TYPE=${1}
 fi
 
+exit 1
+
 # Create personal dirs
 ${ROOT_DIR}/home/init.sh
 
@@ -19,7 +27,7 @@ ${ROOT_DIR}/terminal/setup.sh
 # Setup home
 ${ROOT_DIR}/home/setup.sh
 
-# Gnome config
+# Gnome config - GUI of linux
 if [[ "$DEVICE_TYPE" == "PC" ]]; then
   ${ROOT_DIR}/gnome/setup.sh
 fi
